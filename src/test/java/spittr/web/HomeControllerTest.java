49f4815d5ef656be59dd8828c8a956cd5a04c3a6
grehovGit <grehov@ukr.net>
2017-05-05 19:02:35 +0300

    7 commit

---
+++ src/test/java/spittr/web/HomeControllerTest.java
@@ -0,0 +1,15 @@
+package contacts;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class HomeControllerTest {
+
+  @Test
+  public void testHomePage() throws Exception {
+  }
+
+}
